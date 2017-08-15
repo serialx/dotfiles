176cfe8e3cfd76c508aa550eb09d5636b19b06c6
Brian Hong <sungjinhong@devsisters.com>
2017-08-15 23:49:56 +0900

    Add python.sh, cask.sh

---
+++ cask.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+brew cask install iina
---
+++ python.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew install python3
+pip3 install bpython ipython
+pip3 install nose
+pip3 install requests scapy
+pip3 install scipy scikit-learn pandas matplotlib seaborn jupyter[notebook]
+pip3 install pytorch tensorflow
