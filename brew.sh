59f219d58874f75ef5c7ad41f59b7580eb5d7423
Brian Hong <sungjinhong@devsisters.com>
2017-12-12 00:42:23 +0900

    Update brew.sh to latest package names

--- brew.sh
+++ brew.sh
@@ -29,9 +29,10 @@ brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
 brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+brew install grep
+brew install openssh
+brew install screen
+brew install tmux
 
 brew install bfg
 brew install binutils
