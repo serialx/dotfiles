e6c91d47c3b98772de81c9206d1f62addb68ddda
Brian Hong <sungjinhong@devsisters.com>
2019-07-30 10:27:29 +0900

    Fix golint path

--- golang.sh
+++ golang.sh
@@ -9,7 +9,7 @@ fi
 GO=go
 echo Installing golang tools...
 function go_get_install {
-    $GO get $1
+    $GO get -u $1
     $GO install $1
 }
 go_get_install "github.com/nsf/gocode"
@@ -19,7 +19,7 @@ go_get_install "golang.org/x/tools/cmd/guru"
 go_get_install "golang.org/x/tools/cmd/gorename"
 go_get_install "golang.org/x/tools/cmd/gomvpkg"
 go_get_install "golang.org/x/tools/cmd/cover"
-go_get_install "github.com/golang/lint/golint"
+go_get_install "golang.org/x/lint/golint"
 go_get_install "github.com/kisielk/errcheck"
 go_get_install "github.com/jstemmer/gotags"
 go_get_install "github.com/tools/godep"
